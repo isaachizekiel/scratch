a20291aebe96c57c717b157a186df1fb3884d52c
izak <isaachizekiel@gmail.com>
2022-11-03 11:40:55 +0300

    added comment detail

--- egl_program.c
+++ egl_program.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main() {
-  // this is returning zero
+  // this is returning zero because there is no egl context
   if ( 0 == glCreateProgram()) {
     printf("No egl program\n");
   }
